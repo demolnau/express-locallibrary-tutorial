d073696555ec617b4cbaee20b1ff7e24429cb4f2
Devin <demolnau@iastate.edu>
2023-11-29 15:13:49 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const dotenv = require('dotenv');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require("mongoose");
+const wiki = require("./wiki.js");
+const compression = require("compression");
+const helmet = require("helmet");
+
+// Set path to .env file 
+dotenv.config({ path: './.env' }); 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
+
+var app = express();
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
+//const node_env = process.env.NODE_ENV;
+// Set up mongoose connection
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.DATABASE_URL
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Apply rate limiter to all requests
+app.use(limiter);
+app.use(compression()); // Compress all routes
+app.use("/wiki", wiki);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter); //Add catalog routes to middleware chain
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// // error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
+
+
